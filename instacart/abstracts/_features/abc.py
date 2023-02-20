fb3abdd130702817cf024df0a17b106b82f203d5
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-02-20 18:35:13 +0000

    Updated imports

--- instacart/abstracts/_features/abc.py
+++ instacart/abstracts/_features/abc.py
@@ -1,8 +1,6 @@
 from abc import ABC
-from typing import final
 
 from instacart.interfaces.fabrics.interface import iFabric
-
 from instacart.fabrics.features.catboost.fabric import catBoostClassifierFeaturesFabric
 
 
@@ -11,12 +9,10 @@ class _InstaCartFeaturesAbstractFabric(ABC):
     def __init__(self) -> None:
         self._fabrics: dict = dict()
 
-    @final
     def add_fabric(self, fabric_name: str, fabric: iFabric) -> None:
         if not self._fabrics.get(fabric_name):
             self._fabrics[fabric_name] = fabric
 
-    @final
     def get_fabric(self, fabric_name: str) -> iFabric:
         fabric: iFabric = self._fabrics.get(fabric_name)
         if not fabric:
