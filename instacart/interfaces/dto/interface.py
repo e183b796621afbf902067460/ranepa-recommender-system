4006895db73af9622f5b7d996d7d7c6549df606d
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-02-20 20:44:19 +0000

    Added reduce_mem_usage

--- instacart/interfaces/dto/interface.py
+++ instacart/interfaces/dto/interface.py
@@ -3,6 +3,8 @@ import os
 from typing import List
 import pandas as pd
 
+from instacart.upd.memreduce.upd import reduce_mem_usage
+
 
 NROWS = os.getenv('NROWS', None)
 
@@ -11,7 +13,8 @@ class iDto(ABC):
     _FILTER_COLUMNS: List[str] = None
 
     def __init__(self, csv_file_path: str) -> None:
-        self._raw_df: pd.DataFrame = pd.read_csv(csv_file_path) if not NROWS else pd.read_csv(csv_file_path, nrows=NROWS)
+        self._raw_df: pd.DataFrame = reduce_mem_usage(pd.read_csv(csv_file_path) if not NROWS else pd.read_csv(csv_file_path, nrows=NROWS))
+        self._raw_df.info()
         self._filtered_df: pd.DataFrame = self._raw_df[self._FILTER_COLUMNS]
 
     @property
